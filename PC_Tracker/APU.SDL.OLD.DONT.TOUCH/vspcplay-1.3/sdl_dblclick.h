3f942955aae610170df2a65d9ac69de6e44cc7dd
Bazz <mbazzinotti@gmail.com>
2014-12-26 01:29:48 -0500

    derp. small

--- PC_Tracker/APU.SDL.OLD.DONT.TOUCH/vspcplay-1.3/sdl_dblclick.h
+++ PC_Tracker/APU.SDL.OLD.DONT.TOUCH/vspcplay-1.3/sdl_dblclick.h
@@ -11,12 +11,9 @@ in event loop, for example:
 while (SDL_PollEvent(&ev))
 {
   dblclick::check_event(&ev);
-  switch (ev.type)
-  {
-    case SDL_QUIT:
-    ..
-    .
-  }
+  ...
+  ..
+  .
 }
 
 */
@@ -25,14 +22,19 @@ while (SDL_PollEvent(&ev))
 #pragma once
 #include "SDL.h"
 
-#define DBLCLICK_TIMEWINDOW 800
+#define DBLCLICK_TIMEWINDOW 800 // ms
 
 namespace dblclick
 { 
   SDL_Event trigger_event;
+  // Holds event details at time-of-double-click, since a new USER_EVENT
+  // is sent, the USER_EVENT itself can't hold the mouse events details..
+
   Uint8 numclicks=0;
   SDL_TimerID timerid;
   enum Event { react, reset };
+  
+  // Timer Callback.. Resets double click possibility
   Uint32 dblclicktimer(Uint32 interval, void *param)
   {
     SDL_Event event2;
@@ -51,6 +53,8 @@ namespace dblclick
     timerid = SDL_AddTimer(DBLCLICK_TIMEWINDOW, &dblclicktimer, NULL);
   }
 
+  
+  // You must call this from your Main SDL EVENT LOOP
   void check_event(SDL_Event *ev)
   {
     if (ev->type == SDL_MOUSEBUTTONDOWN)
@@ -81,7 +85,7 @@ namespace dblclick
       }
     }
     else if (ev->type == SDL_USEREVENT)
-      {
+    {
       if (ev->user.code == dblclick::react)
       {
         // could react here or could replicate in main SDL_USEREVENT loop..
