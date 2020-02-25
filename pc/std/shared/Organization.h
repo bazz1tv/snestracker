b5ccf35f12e66489d30ce7d73e300c3f5845476b
Michael Bazzinotti <bazz@bazz1.com>
2020-02-25 12:40:21 -0500

    change app name and version

--- pc/std/shared/Organization.h
+++ pc/std/shared/Organization.h
@@ -2,5 +2,5 @@
 
 #define COMPANY_NAME "Black Hole Studios"
 #define APP_NAME "SNES Tracker"
-#define APP_VERSION "1.33"
+#define APP_VERSION "0.01"
 #define APP_NAME_VERSION APP_NAME " " APP_VERSION
--- pc/std/tracker/globals.h
+++ pc/std/tracker/globals.h
@@ -19,5 +19,5 @@ void handle_error( const char* );
 // 5 minutes default
 #define DEFAULT_SONGTIME  (60*5) 
 
-#define PROG_NAME_VERSION_STRING "SNES Tracker Debugger v%s"
+#define PROG_NAME_VERSION_STRING "SNES Tracker v%s"
 #define CREDITS "APU emulation code from Blargg's GME and Snes_Spc 0.9.0"
