ca3541fc4d63f523d7e637edd4e5ae66c42a43c0
Michael Bazzinotti <bazz@bazz1.com>
2020-03-02 06:18:02 -0500

    [apudriver] reformat dos2unix and spaces to tabs

--- snes_driver/cpu/main.s
+++ snes_driver/cpu/main.s
@@ -19,55 +19,41 @@
 .SECTION "MainCode"
 
 Start:
-    InitSNES    ; Clear registers, etc.
-	
-	
-	
-	; TAKE THIS OUT AFTER DEBuGGING
-	
-	
-	
-    ; Load Palette for our tiles
-    LoadPalette pal, 0, 4
+	InitSNES    ; Clear registers, etc.
 
-    ; Load Tile data to VRAM
-    LoadBlockToVRAM tiles, $0000, tiles_end-tiles	; 2 tiles, 2bpp, = 32 bytes
+	LoadPalette pal, 0, 4
+	; Load Tile data to VRAM
+	LoadBlockToVRAM tiles, $0000, tiles_end-tiles	; 2 tiles, 2bpp, = 32 bytes
 	LoadBlockToVRAM map, $1000, map_end-map
-	
-	
 
-   
-    ; Setup Video modes and other stuff, then turn on the screen
-    jsr SetupVideo
-	
+	; Setup Video modes and other stuff, then turn on the screen
+	jsr SetupVideo
+
 	ldx.w #spx_binary
 	lda #:spx_binary
-	
+
 	stx spx_binary_loc
 	sta spx_binary_loc+2
 	jsr LoadMusic
-  jsr SPCPlaySong
+	jsr SPCPlaySong
 DERP:
 	bra DERP
-  ;jmp TimerOptions
+	;jmp TimerOptions
 
-	
-	
-	
 	.equ brightness_value $0a	; address
 	.equ loop_value $8000
-	
+
 	stz $2100
 	ldy #loop_value
 	ldx.w #showoff_logo
 	stx $10
-	
+
 	;FadeIn 3
 	lda #$01		; add value
-	sta $01	
+	sta $01
 	lda #$0f		; value to compare for
 	sta $02
-	
+
 	ldx #4			; loop for $04ffff
 loop_back:
 	dey
@@ -76,11 +62,11 @@ loop_back:
 	dex
 	cpx #0
 	bne loop_back
-	
+
 	; restore initial loop values for loopback
 	ldy #loop_value
 	ldx #1
-	
+
 	; update brightness settings
 	; $01,$02 control whether the value is added ($01 = 1, $02 = $0f) or subtracted ($01 = $FF, $02 = 0)
 	lda brightness_value
@@ -91,11 +77,11 @@ loop_back:
 	cmp $02
 	beq indirect_jump
 	bra loop_back
-	
+
 indirect_jump:
 	jmp ($10)	; jump to address at $10
-	
-showoff_logo:	
+
+showoff_logo:
 	;Wait 4
 	ldy #$8fff	; loop delay = #$04ffff
 	ldx #8
@@ -108,7 +94,7 @@ wait_loop:
 	cpx #0
 	bne wait_loop
 ; done waiting
-	
+
 	;FadeOut 3
 	ldx.w #enter_tracker	; our destination after fadeout
 	stx $10
@@ -119,7 +105,7 @@ wait_loop:
 	ldy #$ffff
 	ldx #1
 	bra loop_back
-	
+
 enter_tracker:
 	lda #$80
 	sta $2100
@@ -134,7 +120,7 @@ enter_tracker:
 -	sty $2118
 	dex
 	bne -
-	
+
 	ldx #$0000
 	stx $2116
 	lda #$80
@@ -144,21 +130,15 @@ enter_tracker:
 -	sty $2118
 	dex
 	bne -
-	
+
 	; Here we will check for SNESTracker First time Run?? By checking SRAM at Bank 70
-	;lda $700000 ; = First run? 
+	;lda $700000 ; = First run?
 	;bne NotFirstTime
 -
-  bra -
-	
-; We are HERE
-	; VRAM got cleared, it's time to get our Main Menu loaded up
-	
-	
-;
-	
-	
+	bra -
 
+	; We are HERE
+	; VRAM got cleared, it's time to get our Main Menu loaded up
 
 ;============================================================================
 ; SetupVideo -- Sets up the video mode and tile-related registers
@@ -168,22 +148,22 @@ enter_tracker:
 ; Out: None
 ;----------------------------------------------------------------------------
 SetupVideo:
-    lda #$00
-    sta $2105           ; Set Video mode 0, 8x8 tiles, 4 color BG1/BG2/BG3/BG4
+	lda #$00
+	sta $2105           ; Set Video mode 0, 8x8 tiles, 4 color BG1/BG2/BG3/BG4
 
-    lda #4<<2          ; Set BG1's Tile Map offset to $1000 (Word address)
-    sta $2107           ; And the Tile Map size to 32x32
+	lda #4<<2          ; Set BG1's Tile Map offset to $1000 (Word address)
+	sta $2107           ; And the Tile Map size to 32x32
 
-    stz $210B           ; Set BG1's Character VRAM offset to $0000 (word address)
+	stz $210B           ; Set BG1's Character VRAM offset to $0000 (word address)
 
-    lda #$01            ; Enable BG1
-    sta $212C
+	lda #$01            ; Enable BG1
+	sta $212C
 
-    lda #$FF
-    sta $210E
-    stz $210E
+	lda #$FF
+	sta $210E
+	stz $210E
 
-    rts
+	rts
 ;============================================================================
 ; Call with Address stored in $00
 LoadMusic:
@@ -192,130 +172,128 @@ scr_checkready:
 	lda #$AA
 	cmp $2140
 	bne scr_checkready
-  lda #$BB
-  cmp $2141
-  bne scr_checkready
+	lda #$BB
+	cmp $2141
+	bne scr_checkready
 
 	ldy #0
- 
 scr_firstblock:
- 
 	; load spx
 	lda #$01
 	sta $2141
-	
+
 	lda [spx_binary_loc], y
 	iny
 	sta $2142
 	lda [spx_binary_loc], y
 	iny
 	sta $2143
-	
+
 	lda #$CC
 	sta $2140
- 
+
 scr_check2:
 	cmp $2140
 	; check for $CC
 	bne scr_check2
- 
- ; size bytes -> X
- 	rep #$20
+
+	; size bytes -> X
+	rep #$20
 	lda [spx_binary_loc], y
 	tax
 	sep #$20
 	iny
 	iny
-  lda #$FF
+	lda #$FF
 ; byte to transfer
 REDO:
 scr_data_loop:
 ; switch counter to hi-byte
-  xba
+	xba
 ; load a byte
 	lda [spx_binary_loc], y
 	iny
 	sta $2141
 	xba			; counter to lobyte
- 	ina
+	ina
 	sta $2140
-	
+
 	;check port0
 scr_check4:
 	cmp $2140
 	bne scr_check4
- 
+
 	dex
 	bne scr_data_loop
- 
+
 	pha
-  rep #$20
+	rep #$20
 	lda [spx_binary_loc], y
-  beq scr_terminate
+	beq scr_terminate
 	iny
 	iny
-  sep #$20
-  sta $2142
-  xba
-  sta $2143
- 	
- 	; X has starting address
+	sep #$20
+	sta $2142
+	xba
+	sta $2143
+
+	; X has starting address
 	lda #1	; non-zero
 	sta $2141
-  pla
+	pla
 	ina
 scr_nz1:
 	ina
 	beq scr_nz1
- 
+
 	sta $2140
 scr_check5:
 	cmp $2140
 	bne scr_check5
-	
+
 	rep #$20
 	lda [spx_binary_loc], y
 	tax
 	sep #$20
 	iny
 	iny
-  lda #$FF ; reset counter too
+	lda #$FF ; reset counter too
 	bra REDO
- 
+
 scr_terminate:
 	sep #$20
-  sta $2141
+	sta $2141
 	rep #$20
-  iny
-  iny
+	iny
+	iny
 	lda [spx_binary_loc], y
-  sep #$20
+	sep #$20
 	sta $2142
 	xba
-  sta $2143
-  pla
+	sta $2143
+	pla
 	ina
 scr_nz2:
 	ina
 	beq scr_nz2
- 
+
 	sta $2140
 scr_check6:
 	cmp $2140
 	bne scr_check6
- 
+
 	; PROTOCOL COMPLETE.
- 
+
 	RTS
-    
+
 .ENDS
 
 .include "apu/memorymap_defs.i"
 .SECTION "MusicData"
 
 ;.incdir "apu"
-spx_binary: 
-    
+spx_binary:
+
 	.dw SPC_CODE_START, spc_end-spc_start		; start address to upload to in SPC
 spc_start:
 	.INCBIN "apu/spc.bin"
@@ -333,7 +311,7 @@ bass_start:
 	;.INCBIN "sound.brr"
 bass_end:
 
-    .dw $0000, SPC_CODE_START	; 00 to finish transfer, then address to jump SPC700 too to begin code execution
+	.dw $0000, SPC_CODE_START	; 00 to finish transfer, then address to jump SPC700 too to begin code execution
 .ENDS
 
 
@@ -348,7 +326,7 @@ map:
 	.incbin "mainlogo/mainlogo.map"
 map_end:
 tiles:
-    .incbin "mainlogo/mainlogo.pic"
+	.incbin "mainlogo/mainlogo.pic"
 tiles_end:
 pal:
 	.incbin "mainlogo/mainlogo.clr"
