1839c3dcc36038ee8e18a96b9d0e2774d11c9b78
Michael Bazzinotti <bazz@bazz1.com>
2020-03-08 08:37:36 -0400

    sanity practically working

--- pc/std/tracker/Tracker.cpp
+++ pc/std/tracker/Tracker.cpp
@@ -356,8 +356,10 @@ void Tracker::handle_events()
             sound_stop();
             break;
           case UserEvents::callback:
+					{
             void (*p) (void*) = ev.user.data1;
             p(ev.user.data2);
+					}
           break;
 					case UserEvents::report_tracker_incrow:
 						main_window.pateditpanel.inc_currow();
@@ -741,58 +743,7 @@ void Tracker::render_to_apu()
 	// going to check in apu driver for a negative number to mark end
 	// PATTERN SEQUENCER END
 
-	// FOR NOW, EXPORT SPC
-	unsigned char *state = NULL;
-	state = (unsigned char *) SDL_malloc(sizeof(unsigned char) * Snes_Spc::spc_file_size);
-
-  unsigned char* out = state;
-  Snes_Spc::init_header(out);
-
-  // Copy current SPC header info to this new one
-  Spc_Emu::header_t header = ::player->spc_emu()->header();
-  Spc_Emu::header_t *new_header = (Spc_Emu::header_t *) state;
-  memcpy(new_header->song, &header.song, (size_t)((size_t)&new_header->emulator - (size_t)&new_header->song));
-
-	nfdchar_t *outPath=NULL;
-
-	nfdresult_t result = NFD_SaveDialog( "spc", NULL, &outPath );
-	if ( result == NFD_OKAY )
-	{
-		if (outPath !=NULL)
-			fprintf(stderr, "%s\n", outPath);
-
-		bool was_paused = ::player->is_paused();
-		::player->pause(1, false, false);
-		//
-
-		/* Begin writing to spc file */
-		// copy header info
-		unsigned char *out = state;
-		Spc_Emu::header_t *header = (Spc_Emu::header_t *)out;
-
-
-		::player->spc_emu()->can_has_apu()->save_spc(out);
-		::file_system->write_file( outPath, state, Snes_Spc::spc_file_size );
-		// restore player state
-		::player->pause(was_paused, false, false);
-	}
-	else if ( result == NFD_CANCEL ) 
-	{
-		puts("User pressed cancel.");
-	}
-	else
-	{
-		printf("Error: %s\n", NFD_GetError() );
-		result = NFD_ERROR;
-	}
-
-	if (outPath)
-		SDL_free(outPath);
-
-	if (state)
-		SDL_free(state);
-
-
+	
 	// send the command to start the song
 	player->spc_emu()->write_port(1, SPCCMD_PLAYSONG);
 }
