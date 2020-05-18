631f9c58732112170a6fc280530cbebd36aca5ca
Michael Bazzinotti <mbazzinotti@gmail.com>
2020-05-18 14:45:17 -0400

    [submodules] error when make fails

--- build-submodules.sh
+++ build-submodules.sh
@@ -8,15 +8,15 @@ echo Building custom wla-dx
 echo ----------------------
 pushd submodules/wla-dx
 	mkdir -p build-wla && cd build-wla &&
-	cmake .. && cmake --build . --config Debug
+	cmake .. && cmake --build . --config Debug || echo "CRITICAL ERROR" && exit 1
 popd
 
 echo
 echo Building pcx2snes
 echo -----------------
-make -C submodules/pcx2snes
+make -C submodules/pcx2snes || echo "CRITICAL ERROR" && exit 1
 echo
 
 echo Building libgme_m
 echo -----------------
-make -C submodules/libgme_m
+make -C submodules/libgme_m || echo "CRITICAL ERROR" && exit 1
