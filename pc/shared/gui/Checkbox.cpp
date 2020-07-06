354a41647704dea91856507334ccf03b14a0a054
Michael Bazzinotti <mbazzinotti@gmail.com>
2020-07-06 12:51:32 -0400

    tentatively add checkbox class

---
+++ pc/shared/gui/Checkbox.cpp
@@ -0,0 +1,64 @@
+#include "gui/Checkbox.h"
+#include "utility.h"
+#include "DEBUGLOG.h"
+#include "shared/mouse.h" // for mouse coords
+#include "shared/sdl_userevents.h"
+
+Checkbox::Checkbox(bool *state/*=NULL*/, int (*action)(void *data)/*=NULL*/, void *data/*=NULL*/) :
+    Clickable_Rect(action, data),
+    state(state), almost_clicked(0)
+{
+
+}
+
+void Checkbox::check_event(const SDL_Event &ev)
+{
+  //if (!enabled)
+    //return;
+  if (ev.type == SDL_MOUSEBUTTONDOWN &&
+      (Utility::coord_is_in_rect(ev.button.x, ev.button.y, &rect)) &&
+      ev.button.button == SDL_BUTTON_LEFT)
+  {
+    DEBUGLOG("Checkbox mouseCheckboxdown; ");
+    almost_clicked = 1;
+  }
+  else if (state && *state == ON)
+  {
+    if (!Utility::coord_is_in_rect(mouse::x, mouse::y, &rect))
+    {
+      almost_clicked = 0;
+    }
+    else if (ev.type == SDL_MOUSEBUTTONUP)
+    {
+      DEBUGLOG("Checkbox MouseCheckboxup; ");
+      if (almost_clicked)
+      {
+        check_mouse_and_execute(mouse::x, mouse::y, &rect);
+        state ? *state = !*state;
+        almost_clicked = 0;
+      }
+      else
+      {
+        DEBUGLOG("checkbox: mouse button released here but no mousebuttondown here first\n");
+      }
+
+    }
+  }
+}
+
+void Checkbox::draw(SDL_Surface *screen)
+{
+  //DEBUGLOG("Checkbox::draw; ");
+  // redudant calculating outer rect every frame :(
+  inner = {rect.x + 2, rect.y + 2, rect.w - 4, rect.h - 4};
+  SDL_DrawRect(screen, &rect, Colors::white);
+
+  if (state && *state == ON)
+  {
+    SDL_FillRect(screen, &inner, Colors::gray);
+  }
+  else
+  {
+    SDL_FillRect(screen, &inner, Colors::transparent);
+  }
+}
---
+++ pc/shared/gui/Checkbox.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "gui/Clickable_Rect.h"
+
+struct Checkbox : public Clickable_Rect
+{
+  /* The difference Button from Clickable_Text is as follows:
+   * Event handler code that adjusts the drawing when the left mouse
+   * button is held down. The adjustments are a slightly lower vertical
+   * text position (perhaps also slightly right horiz position), and a
+   * darker background color. They also receive a callback function to
+   * call when the mouse button is released.*/
+  Checkbox(bool *state=NULL, int (*action)(void *data)=NULL, void *data=NULL);
+  
+  void check_event(const SDL_Event &ev);
+
+  void draw(SDL_Surface *screen);
+
+  SDL_Rect inner;
+
+private:
+  /* Based on the below state, alternative colors and positioning will
+   * effect in draw() */
+  enum
+  {
+  	OFF=0
+    ON=1,
+  };
+
+  bool *state;
+  unsigned int almost_clicked :1;
+};
