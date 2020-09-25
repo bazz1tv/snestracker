b25b55175e1679806469d49824576b688f065dad
Michael Bazzinotti <bazz@bazz1.com>
2020-09-25 16:44:42 -0400

    [Text] Allow control of background color

--- pc/shared/gui/Text.cpp
+++ pc/shared/gui/Text.cpp
@@ -14,9 +14,11 @@ void Text::init_width_height()
 }
 
 void Text::draw(SDL_Surface *screen/*=RenderContext::screen*/,
-  Uint32 color/*=Colors::Interface::color[Colors::Interface::Type::text_fg]*/,
-  bool prefill/*=true*/, bool Vflip/*=false*/, bool Hflip/*=false*/)
+  Uint32 fg_color/*=Colors::Interface::color[Colors::Interface::Type::text_fg]*/,
+  bool prefill/*=true*/, bool Vflip/*=false*/, bool Hflip/*=false*/,
+  Uint32 bg_color/*=Colors::Interface::color[Colors::Interface::Type::text_bg]*/
+)
 {
-  sdlfont_drawString(screen, rect.x, rect.y, str, color,
-    Colors::Interface::color[Colors::Interface::Type::text_bg], prefill, Vflip, Hflip);
+  sdlfont_drawString(screen, rect.x, rect.y, str, fg_color,
+    bg_color, prefill, Vflip, Hflip);
 }
--- pc/shared/gui/Text.h
+++ pc/shared/gui/Text.h
@@ -8,9 +8,10 @@ struct Text
   Text(const char *str = "");
   void init_width_height();
   void draw(SDL_Surface *screen=::render->screen,
-      Uint32 color=Colors::Interface::color[Colors::Interface::Type::text_fg],
+      Uint32 fg_color=Colors::Interface::color[Colors::Interface::Type::text_fg],
       bool prefill=true, bool Vflip=false,
-      bool Hflip=false);
+      bool Hflip=false,
+      Uint32 bg_color=Colors::Interface::color[Colors::Interface::Type::text_bg]);
   /* defines the rect of this text entity. Only the coords are used
    * currently; the width and height are merely informational */
   SDL_Rect rect;
