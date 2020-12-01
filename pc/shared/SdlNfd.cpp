c3f9deb6d2b9df414858635af636d69ba67246c8
Bazz <bazz@bazz1.com>
2020-11-30 22:02:57 -0500

    fixes #128
    [nfd] Automatic appending of file extension to saved files

--- pc/shared/SdlNfd.cpp
+++ pc/shared/SdlNfd.cpp
@@ -1,4 +1,5 @@
 #include "SdlNfd.h"
+#include "DEBUGLOG.h"
 
 nfdchar_t *SdlNfd::outPath = NULL;
 SDL_RWops *SdlNfd::file = NULL;
@@ -17,7 +18,9 @@ int SdlNfd::init(SDL_Window *win)
   _sdlWindow = win;
 }
 
-
+/* rw: "r" or "rb" read
+       "w" or "wb" write
+*/
 nfdresult_t SdlNfd::get_file_handle(const char *rw, const char *filter_list/*=NULL*/)
 {
   if (_sdlWindow == NULL)
@@ -32,11 +35,18 @@ nfdresult_t SdlNfd::get_file_handle(const char *rw, const char *filter_list/*=NU
   char tmpbuf[200];
   outPath=NULL;
   nfdresult_t result;
+  bool saving = false;          // track whether we're doing a file read or a file write
 
   if (!strcmp(rw, "r") || !strcmp(rw, "rb"))
+  {
+    saving = false;
     result = NFD_OpenDialog( filter_list, NULL, &outPath );
+  }
   else if (!strcmp(rw, "w") || !strcmp(rw, "wb"))
+  {
+    saving = true;
     result = NFD_SaveDialog( filter_list, NULL, &outPath );
+  }
   else
   {
     fprintf(stderr, "Unsupported file mode '%s', ignoring...\n", rw);
@@ -54,7 +64,55 @@ nfdresult_t SdlNfd::get_file_handle(const char *rw, const char *filter_list/*=NU
 
   if ( result == NFD_OKAY )
   {
-    file = SDL_RWFromFile(outPath, rw);
+    char *finalPath = outPath;
+    // If we're saving a file, we need to check if the file extension was present in the outPath, and append if not.
+    if (saving)
+    {
+      DEBUGLOG("in Saving\n");
+      // Before we get the handle on that path, we're going to
+      // (Get the first file extension in the filter list)
+#     define MAX_EXT 10
+      char extensionBuf[MAX_EXT]; // just trying to be generally safe here and allow an extension up to 9 characters long.
+      // copy the first extension in the filter_list into the extensionBuf
+      /* The will either stop at the first comma delimiter found, or it will stop when encountering the null char
+       * You must manually finish off the string with a null char after the loop */
+
+      /* OK, it IS possible that the filter list entries could have an extension longer than my general MAX_EXT defined above.
+       * But, I am in control of the filter_lists, not the end user. So we're good! We're not going make a
+       * 10 character file extension LUL */
+
+      extensionBuf[0] = '.';
+      // copy the first entry of the filter_list into extensionBuf
+      {
+        int i=0;
+        for(; filter_list[i] != ',' && filter_list[i] != 0; i++)
+          extensionBuf[i+1] = filter_list[i];
+        extensionBuf[i+1] = 0;
+        // so now extensionBuf has eg. ".sts\0"
+
+        DEBUGLOG("extensionBuf: \"%s\"\n", extensionBuf);
+      }
+
+      // check if the file extension is in outPath.
+      if (strstr(outPath, extensionBuf) == NULL)
+      {
+        DEBUGLOG("File extension is NOT present\n");
+        finalPath = malloc (strlen(outPath) + 1 + strlen(extensionBuf) + 1); // the plus 1's are the '.' and the null char
+        // append that extension to a new string
+        strcpy(finalPath, outPath);
+        strcat(finalPath, extensionBuf);
+      }
+      else
+      {
+        DEBUGLOG("EXTENSION was present\n");
+      }
+      // else, outPath already has the extension, and we don't need to do anything.
+    }
+
+    // IF NOT, append that file extension  to the path string
+
+    //and then get a handle on THAT
+    file = SDL_RWFromFile(finalPath, rw);
 
     if (file == NULL)
     {
@@ -64,12 +122,27 @@ nfdresult_t SdlNfd::get_file_handle(const char *rw, const char *filter_list/*=NU
                      "Could not open FILE!",
                      tmpbuf,
                      NULL);
+
+      // Free finalPath here
+      if (finalPath != outPath) // only free if it was allocated
+      {
+        free(finalPath);
+        finalPath = NULL;
+      }
+
       if (outPath)
       {
         free(outPath);
         outPath = NULL;
       }
       return NFD_ERROR;
+    }
+
+    // Free finalPath here
+    if (finalPath != outPath) // only free if it was allocated
+    {
+      free(finalPath);
+      finalPath = NULL;
     }
     return result;
   }
